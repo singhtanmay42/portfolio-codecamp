07f7d54680298e00625ed983f2cb190c900d0cb9
tanmay singh <singhtanmay42@gmail.com>
2018-10-21 18:30:59 +0530

    backend commit

--- portfolio/blog/models.py
+++ portfolio/blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class blog(models.Model):
+    image = models.ImageField(upload_to='images/')
+    summary = models.CharField(max_length=200)
+    body = models.TextField()
+    pub_date = models.DateTimeField(auto_now=False, auto_now_add= False)
+     
